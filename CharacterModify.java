47cfce1c9658cc9a0e37254b5c02d65532d327e6
Tatenda Usuwana Mapuranga <49365479+Usuwana@users.noreply.github.com>
2020-05-18 21:38:18 +0800

    Initial character modify class

--- CharacterModify.java
+++ CharacterModify.java
@@ -1,6 +1,49 @@
 import java.util.*;
 
-public class CharacterModify implements GameCharacter 
+public abstract class CharacterModify implements GameCharacter 
 {
-    
+    protected GameCharacter next;
+
+    public CharacterModify(GameCharacter next)
+    {
+        this.next = next;
+    }
+
+    @Override
+    public void getName() 
+    {
+        next.getName();
+    }
+
+
+    @Override
+    public Object getWeapon() 
+    {
+        return next.getWeapon();
+    }
+
+    @Override
+    public int getMaxHealth() 
+    {
+        return next.getMaxHealth();
+    }
+
+    @Override
+    public int getCurrHealth() 
+    {
+        return next.getCurrHealth();
+    }
+
+    @Override
+    public Object getArmour() 
+    {
+        return next.getArmour();
+    }
+
+    @Override
+    public int getGold() 
+    {
+        return next.getGold();
+    }
+
 }
